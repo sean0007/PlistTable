59e0554805ca9f2446c233deade67875c6c7da0f
sean0007 <sean77ryan@gmail.com>
2016-02-26 17:37:38 +1100

    PlistTableViewSugue
    segue not working

---
+++ PlistTable/name.swift
@@ -0,0 +1,16 @@
+//
+//  name.swift
+//  PlistTable
+//
+//  Created by sean ryan on 26/02/2016.
+//  Copyright © 2016 sean ryan. All rights reserved.
+//
+
+import Foundation
+
+
+struct Name{
+    
+    let personName : String
+    
+}
